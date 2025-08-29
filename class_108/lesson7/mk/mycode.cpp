904d49752704546c34c3a5465bd485404ab3a817
Jason-zrh <renhe.zhang@hotmail.com>
2025-08-29 17:22:10 +0800

    8-29

--- class_108/lesson7/mk/mycode.cpp
+++ class_108/lesson7/mk/mycode.cpp
@@ -11,11 +11,14 @@ int main()
     vector<int> prefix;
     prefix.resize(n + 1);
     for(int i = 0; i < n; i++)
+    {
         cin >> prefix[i];
+    }
 
     for(int i = 0; i < n; i++)
+    {
         cout << prefix[i] << " ";
-
+    }
     cout << endl;
     return 0;
 }
