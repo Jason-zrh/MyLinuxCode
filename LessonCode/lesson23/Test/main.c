25f08b830860f1a705abe13fe94883baf2b07191
Jason-zrh <renhe.zhang@hotmail.com>
2025-10-20 21:57:06 +0800

    10-20

---
+++ LessonCode/lesson23/Test/main.c
@@ -0,0 +1,8 @@
+#include "mymath.h"
+
+int main()
+{
+    printf("1 + 1 = %d\n", add(1, 1));
+
+    return 0;
+}
---
+++ LessonCode/lesson23/mymath.c
@@ -0,0 +1,28 @@
+#include "mymath.h"
+
+int errorno = 0;
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int sub(int x, int y)
+{
+    return x - y;
+}
+
+int mul(int x, int y)
+{
+    return x * y;
+}
+
+int div(int x, int y)
+{
+    if(y == 0)
+    {
+        errorno = -1;
+        return -1;
+    }
+    return x / y;
+}
